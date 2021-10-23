0040a78428ff50306b14b6250cab76ac31086f50
paracelso93 <68126328+paracelso93@users.noreply.github.com>
2021-10-23 17:43:21 +0200

    Removed useless duplicate code in memory.c

--- src/utils/memory.c
+++ src/utils/memory.c
@@ -54,24 +54,6 @@ void print_memory_stats() {
             amount_allocated = (f32)allocated;
             amount_freed = (f32) freed;
         }
-        if (allocated >= gib) {
-            unit[0] = 'G';
-            amount_allocated = allocated / (f32)gib;
-            amount_freed = freed / (f32)gib;
-        } else if (allocated >= mib) {
-            unit[0] = 'M';
-            amount_allocated = allocated / (f32)mib;
-            amount_freed = freed / (f32)mib;
-        } else if (allocated >= kib) {
-            unit[0] = 'k';
-            amount_allocated = allocated / (f32)kib;
-            amount_freed = freed / (f32)kib;
-        } else {
-            unit[0] = 'b';
-            unit[1] = '\0';
-            amount_allocated = (f32)allocated;
-            amount_freed = (f32) freed;
-        }
         bdebug("%s: %.2f%s - %.2f%s -> %.2f%s", tags[i], amount_allocated, unit, amount_freed, unit, amount_allocated - amount_freed, unit);;
     }
     printf("\n");
